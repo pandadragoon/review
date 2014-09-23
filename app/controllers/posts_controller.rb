04a0cd1229099a5f449e32f72dc36bbc1692ff6a
Joshua Edwards <pandadragoon@gmail.com>
2014-09-23 14:11:43 -0500

    added update and destroy for reviews

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,9 +24,23 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
   def update
     @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:notice] = "Your review was updated"
+      redirect_to root_path
+    else
+      render :new
+    end
 
+  end
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to root_path
   end
 end
 
